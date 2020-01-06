aac780fdb9dfccf7951af6333a07a9d8f238841a
Mikael Kaya <kayaswork@gmail.com>
2020-01-06 04:35:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,16 @@
 class CashRegister
   CashRegister = CashRegister.new
 end
+
+class Person
+ 
+  attr_accessor :age
+ 
+  def initialize(age = 0)
+    @age = age 
+  end 
+ 
+  def birthday
+    self.age += 1
+  end
+end
