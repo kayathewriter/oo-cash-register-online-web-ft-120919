56e9df6a2e97d7b83c563f3ca4872f7dcba518e2
Mikael Kaya <kayaswork@gmail.com>
2020-01-06 04:33:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,4 +1,6 @@
-
+class CashRegister' do
+  let(:cash_register) { CashRegister.new }
+  let(:cash_register_with_discount) { CashRegister.new(20) }
 
 lassie = Dog.new("Collie")
 lassie.breed #=> "Collie"
