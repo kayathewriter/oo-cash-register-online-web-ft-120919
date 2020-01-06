225e7338c9ef327394745a81a879817af2c59946
Mikael Kaya <kayaswork@gmail.com>
2020-01-06 04:33:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,17 +1,4 @@
-class Person
- 
-  attr_accessor :age
- 
-  def initialize(age = 0)
-    @age = age 
-  end 
- 
-  def birthday
-    self.age += 1
-  end
-end
 
-def apply_discount
-     @cash_register_with_discount = @cash_register_with_discount/100.to_f
-     @total = @total - (@total * @cash_register_with_discount)
-   end
+
+lassie = Dog.new("Collie")
+lassie.breed #=> "Collie"
